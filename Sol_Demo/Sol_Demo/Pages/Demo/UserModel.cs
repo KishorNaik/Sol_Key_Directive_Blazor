b61a76c09084f71b9b765ea30af317529681f3b2
KishorNaik <kishor.naik011.net@gmail.com>
2020-04-13 22:22:13 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Pages/Demo/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Pages.Demo
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+
+        public String FullName { get; set; }
+    }
+}
